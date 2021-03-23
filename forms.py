7b1cf9d6b7584836b83df916445369de92e179f4
alicekykwan <62489895+alicekykwan@users.noreply.github.com>
2021-03-23 01:55:18 -0700

    enum for state and genres

--- forms.py
+++ forms.py
@@ -3,6 +3,7 @@ from flask_wtf import FlaskForm as Form
 from wtforms import StringField, SelectField, SelectMultipleField, DateTimeField, BooleanField, IntegerField
 from wtforms.validators import DataRequired, AnyOf, URL, Optional
 import re
+from enums import Genre, State
 
 def is_valid_phone(number):
     """ Validate phone numbers like:
@@ -20,84 +21,6 @@ def is_valid_phone(number):
     """
     regex = re.compile('^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$')
     return regex.match(number)
-
-    # if not re.search(r'^[1-9]\d{2}-\d{3}-\d{4}$', field.data):
-    #     raise ValidationError("error: Phone number should only contain digits (xxx-xxx-xxxx)")
-
-state_choices = [
-            ('AL', 'AL'),
-            ('AK', 'AK'),
-            ('AZ', 'AZ'),
-            ('AR', 'AR'),
-            ('CA', 'CA'),
-            ('CO', 'CO'),
-            ('CT', 'CT'),
-            ('DE', 'DE'),
-            ('DC', 'DC'),
-            ('FL', 'FL'),
-            ('GA', 'GA'),
-            ('HI', 'HI'),
-            ('ID', 'ID'),
-            ('IL', 'IL'),
-            ('IN', 'IN'),
-            ('IA', 'IA'),
-            ('KS', 'KS'),
-            ('KY', 'KY'),
-            ('LA', 'LA'),
-            ('ME', 'ME'),
-            ('MT', 'MT'),
-            ('NE', 'NE'),
-            ('NV', 'NV'),
-            ('NH', 'NH'),
-            ('NJ', 'NJ'),
-            ('NM', 'NM'),
-            ('NY', 'NY'),
-            ('NC', 'NC'),
-            ('ND', 'ND'),
-            ('OH', 'OH'),
-            ('OK', 'OK'),
-            ('OR', 'OR'),
-            ('MD', 'MD'),
-            ('MA', 'MA'),
-            ('MI', 'MI'),
-            ('MN', 'MN'),
-            ('MS', 'MS'),
-            ('MO', 'MO'),
-            ('PA', 'PA'),
-            ('RI', 'RI'),
-            ('SC', 'SC'),
-            ('SD', 'SD'),
-            ('TN', 'TN'),
-            ('TX', 'TX'),
-            ('UT', 'UT'),
-            ('VT', 'VT'),
-            ('VA', 'VA'),
-            ('WA', 'WA'),
-            ('WV', 'WV'),
-            ('WI', 'WI'),
-            ('WY', 'WY'),
-        ]
-genres_choices = [
-            ('Alternative', 'Alternative'),
-            ('Blues', 'Blues'),
-            ('Classical', 'Classical'),
-            ('Country', 'Country'),
-            ('Electronic', 'Electronic'),
-            ('Folk', 'Folk'),
-            ('Funk', 'Funk'),
-            ('Hip-Hop', 'Hip-Hop'),
-            ('Heavy Metal', 'Heavy Metal'),
-            ('Instrumental', 'Instrumental'),
-            ('Jazz', 'Jazz'),
-            ('Musical Theatre', 'Musical Theatre'),
-            ('Pop', 'Pop'),
-            ('Punk', 'Punk'),
-            ('R&B', 'R&B'),
-            ('Reggae', 'Reggae'),
-            ('Rock n Roll', 'Rock n Roll'),
-            ('Soul', 'Soul'),
-            ('Other', 'Other'),
-        ]
 
 class ShowForm(Form):
     artist_id = IntegerField(
@@ -121,10 +44,10 @@ class VenueForm(Form):
         if not is_valid_phone(self.phone.data):
             self.phone.errors.append('Invalid phone.')
             return False
-        if not set(self.genres.data).issubset(dict(genres_choices).keys()):
+        if not set(self.genres.data).issubset(dict(Genre.choices()).keys()):
             self.genres.errors.append('Invalid genres.')
             return False
-        if self.state.data not in dict(state_choices).keys():
+        if self.state.data not in dict(State.choices()).keys():
             self.state.errors.append('Invalid state.')
             return False
         # if pass validation
@@ -138,7 +61,7 @@ class VenueForm(Form):
     )
     state = SelectField(
         'state', validators=[DataRequired()],
-        choices=state_choices
+        choices=State.choices()
     )
     address = StringField(
         'address', validators=[DataRequired()]
@@ -153,7 +76,7 @@ class VenueForm(Form):
     genres = SelectMultipleField(
         # TODO implement enum restriction
         'genres', validators=[DataRequired()],
-        choices=genres_choices
+        choices=Genre.choices()
     )
     facebook_link = StringField(
         'facebook_link', validators=[Optional(), URL()]
@@ -179,10 +102,10 @@ class ArtistForm(Form):
         if not is_valid_phone(self.phone.data):
             self.phone.errors.append('Invalid phone.')
             return False
-        if not set(self.genres.data).issubset(dict(genres_choices).keys()):
+        if not set(self.genres.data).issubset(dict(Genre.choices()).keys()):
             self.genres.errors.append('Invalid genres.')
             return False
-        if self.state.data not in dict(state_choices).keys():
+        if self.state.data not in dict(State.choices()).keys():
             self.state.errors.append('Invalid state.')
             return False
         # if pass validation
@@ -195,7 +118,7 @@ class ArtistForm(Form):
     )
     state = SelectField(
         'state', validators=[DataRequired()],
-        choices=state_choices
+        choices=State.choices()
     )
     phone = StringField(
         # TODO implement validation logic for state
@@ -206,7 +129,7 @@ class ArtistForm(Form):
     )
     genres = SelectMultipleField(
         'genres', validators=[DataRequired()],
-        choices=genres_choices
+        choices=Genre.choices()
      )
     facebook_link = StringField(
         # TODO implement enum restriction
